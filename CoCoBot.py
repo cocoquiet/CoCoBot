6ca49fb5b8d1bd1ec723f0c0af6f39ddc7209666
JiSeong Lee <52674782+easycastle@users.noreply.github.com>
2020-12-09 20:38:45 +0900

    Update CoCoBot.py

--- CoCoBot.py
+++ CoCoBot.py
@@ -168,7 +168,7 @@ async def playlist(ctx):
 
 @bot.command(name="실검")
 async def searchterm(ctx):
-    headers = {'User-Agent':'Chrome/80.0.3987.122'}
+    headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.362'}
     url = 'https://datalab.naver.com/keyword/realtimeList.naver?where=main'
     res = requests.get(url, headers = headers)
     soup = BeautifulSoup(res.content, 'html.parser')
@@ -178,7 +178,8 @@ async def searchterm(ctx):
     
     i = 1
     for item in data:
-        embed.add_field(name=str(i) + "위", value=item.get_text(), inline=False)
+        link = item.get_text().replace(" ", "+")
+        embed.add_field(name=str(i) + "위", value="[" + item.get_text() + "](https://search.naver.com/search.naver?query=" + link + ")", inline=False)
         i += 1
 
     await ctx.send(embed=embed)
