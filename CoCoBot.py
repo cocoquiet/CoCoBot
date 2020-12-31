b0fdac923d390355cf39c3aa49ac8f48ce02cdeb
easycastle <sean34418290@gmail.com>
2020-12-31 12:33:32 +0900

    깃허브 링크 기능 추가

--- CoCoBot.py
+++ CoCoBot.py
@@ -151,6 +151,21 @@ async def date(ctx):
 
     await ctx.send("오늘은 " + nowYear + "년 " + nowMonth + "월 " + nowDay + "일 입니다!")
 
+@bot.command(name="깃허브")
+async def github(ctx):
+    embed = discord.Embed(title="관리자 깃허브 링크 목록", description="깃허브 링크 모음입니다.", color=0x00ff00)
+    embed.add_field(name="```코양이 위원장```", value="https://github.com/easycastle", inline=False)
+    embed.add_field(name="```양사```", value="https://github.com/sat0317", inline=False)
+    embed.add_field(name="```러리```", value="https://github.com/Coalery", inline=False)
+    embed.add_field(name="```Cpp고수```", value="https://github.com/cpprhtn", inline=False)
+    embed.add_field(name="```깃고```", value="https://github.com/NewPremium", inline=False)
+    embed.add_field(name="```암고```", value="https://github.com/azure-06", inline=False)
+    embed.add_field(name="```루```", value="https://github.com/Lu175", inline=False)
+    embed.add_field(name="```뽀로로```", value="https://github.com/paxbun", inline=False)
+    embed.add_field(name="```녹색치킨```", value="https://github.com/IceJack", inline=False)
+
+    await ctx.send(embed=embed)
+
 @bot.command(name="음악목록")
 async def playlist(ctx):
     embed = discord.Embed(title="코양이 유튜브 재생목록", description="유튜브 재생목록 모음입니다.", color=0x00ff00)
