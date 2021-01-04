f3e9b1f12558a7c928f95fd0a27cbb39a86d95b3
easycastle <sean34418290@gmail.com>
2021-01-04 22:22:58 +0900

    자스고 강퇴 테스트 추가

--- cogs/secret.py
+++ cogs/secret.py
@@ -10,13 +10,11 @@ class Secret(commands.Cog):
     async def test(self, ctx):
         await ctx.send("설빙 시험 화이팅!!!!!!!!")
 
-    @commands.command(name="신년")
-    async def test(self, ctx):
-        await ctx.send("다들 새해 복 많이 받으세요!!!!!")
-
     @commands.command(name="자스고")
-    async def test(self, ctx):
-        await ctx.send(":AMGO_sad: :AMGO_peaceful: :AMGO_dispire: :AMGO_angry:")
+    @commands.has_permissions(administrator=True)
+    async def jasgo(self, ctx):
+        member = "CoCoTestBot#7796"
+        await member.kick()
 
 def setup(bot):
     bot.add_cog(Secret(bot))
