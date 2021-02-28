40bfa7d7c0ea32fdb4025a75ad23139f09c4dd86
easycastle <sean34418290@gmail.com>
2021-03-01 06:13:39 +0900

    CoCo 코코야 접두사 제거형 추가

--- cogs/CoCo.py
+++ cogs/CoCo.py
@@ -4,19 +4,28 @@ from discord.ext import commands
 
 import random
 
+CoCoResponse = ["네?", "뭐", "왜", "ㅇㅇ?", "누가 불렀니", "아 왜 불러ㅡㅡ"]
+
 class CoCo(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+    
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        global CoCoResponse
+        
+        if (message.content == "코코야"):
+            await message.channel.send(CoCoResponse[random.randint(0, 6)])
+
+    @commands.command(name="코코야", aliases=["hey", "야"])
+    async def HeyYou(self, ctx):
+        global CoCoResponse
+
+        await ctx.send(CoCoResponse[random.randint(0, 6)])
 
     @commands.command(name="안녕", aliases=["hi", "hello", 'h'])
     async def hello(self, ctx):
         await ctx.send("안녕하세요 " + ctx.message.author.mention + " 님!")
-
-    @commands.command(name="코코야", aliases=["hey", "야"])
-    async def HeyYou(self, ctx):
-        CoCoResponse = ["네?", "뭐", "왜", "ㅇㅇ?", "누가 불렀니", "아 왜 불러ㅡㅡ"]
-        
-        await ctx.send(CoCoResponse[random.randint(0, 6)])
 
     @commands.command(name="짖어", aliases=["bark", "멍멍"])
     async def bark(self, ctx):
