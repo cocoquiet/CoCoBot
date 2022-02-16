8a43d5c9c4a59d3af78e448f6013d77062432cb9
easycastle <sean34418290@gmail.com>
2022-02-17 00:20:09 +0900

    fix: secret slash_command

--- cogs/secret.py
+++ cogs/secret.py
@@ -7,12 +7,12 @@ class Secret(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.slash_command(name='설빙')
+    @slash_command(name='설빙')
     async def sul(self, ctx):
         """"""
         await ctx.respond('설빙님 화이팅!!!!!!!!')
 
-    @commands.slash_command(name='러리')
+    @slash_command(name='러리')
     async def lery(self, ctx):
         """"""
         await ctx.respond('러리님 과제 화이팅하세요!!!!!!!!!!')
--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 botName = 'CoCoBot'
 botID = '758507966965350420'
-EXTENSIONS = ['cogs.help', 'cogs.CoCo', 'cogs.admin', 'cogs.music', 'cogs.RSP', 'cogs.omok', 'cogs.util', 'cogs.crawling', 'cogs.github', 'cogs.ping', 'cogs.secret']
+EXTENSIONS = ['cogs.help', 'cogs.CoCo', 'cogs.admin', 'cogs.music', 'cogs.RSP', 'cogs.omok', 'cogs.util', 'cogs.github', 'cogs.ping', 'cogs.secret']
 
 CoCoColor = 0xFFFFFE
 CoCoVER = '코코#1174 | V2.0'
