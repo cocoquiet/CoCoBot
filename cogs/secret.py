7cce839332f1200cbf728056f0ae4cf40671cf36
easycastle <sean34418290@gmail.com>
2022-02-25 15:22:22 +0900

    feat: Add secret yangsa

--- cogs/secret.py
+++ cogs/secret.py
@@ -3,7 +3,7 @@ import asyncio
 from discord.ext.commands import Cog
 from discord.commands import slash_command
 
-from datetime import date, datetime
+from datetime import datetime
 
 class Secret(Cog):
     def __init__(self, bot):
@@ -28,6 +28,14 @@ class Secret(Cog):
         """암고"""
         
         await ctx.respond('암고님 공부하세요!!!')
+
+    @slash_command(name='양사')
+    async def yangsa(self, ctx):
+        """양사"""
+        
+        remainder = (datetime.strptime('20221117', '%Y%m%d') - datetime.now()).days + 1
+        
+        await ctx.respond(f'양사님 수능까지 {remainder}일 남았습니다!!!!!')
 
 def setup(bot):
     bot.add_cog(Secret(bot))
